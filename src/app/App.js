ebfc9703a6b22b7bf45d1c7ffe67fc6f897da490
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2018-11-22 14:30:38 +0300

    manipulating componentes

--- src/app/App.js
+++ src/app/App.js
@@ -3,22 +3,65 @@ import { Header } from './components/Header'
 import { Home} from './components/Home'
 
 class App extends Component {
-    render() {
-        var user = {
-            name: "Inno",
-            hobbies: ["Sports", "Fifa"]
+    constructor() {
+        super();
+        this.state = {
+            homeLink: "Home",
+            homeMounted: true
         };
+    }
+    onChangeHomeMounted= () => {
+        this.setState({
+            homeMounted: !this.state.homeMounted
+        });
+    }
+    onGreet() {
+        alert("Hello")
+    }
+
+    onChangeLinkName = (newName) => {
+        this.setState({
+            homeLink: newName
+        });
+    }
+
+    render() {
+        let homeCmp = "";
+        if (this.state.homeMounted) {
+            homeCmp = (
+                <Home 
+                    name={"Jude"} 
+                    initialAge={100} 
+                    greet={this.onGreet}
+                    changeLink={this.onChangeLinkName}
+                    initialLink={this.state.homeLink}
+                />
+            )
+        }
         return (<div className="container">
                     <div className="row">
                         <div className="col-xs-10 col-xs-offset-1">
-                            <Header/>
+                            <Header homeLink={this.state.homeLink}/>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-xs-10 col-xs-offset-1">
-                            <Home name={"Jude"} age={"100"} user={user}>
-                                <p>This is a paragrath</p>
-                            </Home>
+                            {homeCmp
+                                /* <Home 
+                                name={"Jude"} 
+                                initialAge={100} 
+                                greet={this.onGreet}
+                                changeLink={this.onChangeLinkName}
+                                initialLink={this.state.homeLink}
+                            /> */}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-10 col-xs-offset-1">
+                            <button onClick={this.onChangeHomeMounted} 
+                                type="button" 
+                                className="btn btn-dark">(Un)Mount Home cmp
+                            </button>
                         </div>
                     </div>
                 </div>
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,21 +2,86 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            age: props.initialAge,
+            status: 0,
+            homeLink: props.initialLink
+        };
+        setTimeout(() => {
+            this.setState({
+                status: 1
+            });
+        }, 3000);
+        console.log("Constactor");
+    }
+    componentWillMount() {
+        console.log("componentWillMount");
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log("componentWillReceiveProps", nextProps);
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("shouldComponentUpdate", nextProps, nextState);
+        // if (nextState.status == 1) {
+        //     return false 
+        // }
+        return true
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log("componentWillUpdate", nextProps, nextState);
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("componentDidUpdate", prevProps, prevState);
+
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+
+    }
+
+    onMakeOlder = () => {
+        this.setState({
+            age: this.state.age + 3
+        });
+    }
+    onChangeLink = () => {
+        this.props.changeLink(this.state.homeLink);
+        console.log(this.state.homeLink);
+    }
+    onHandleChange = (event) => {
+        this.setState({
+            homeLink: event.target.value
+        });
+    }
     render() {
-        console.log(this.props);
         return (
             <div>
                 <p>In a new component</p>
-                <p>Your name is {this.props.name} and your age is {this.props.age}</p>
-                <p>User Object => Name: {this.props.user.name}</p>
-                <div>
-                    <h4>Hobbies</h4>
-                    <ul>
-                        {this.props.user.hobbies.map((hobby, i) => <li key={i }>{hobby}</li>)}
-                    </ul>
-                    <hr/>
-                    {this.props.children}
-                </div>
+                <p>Your name is {this.props.name} and your age is {this.state.age}</p>
+                <hr/>
+                <button onClick={this.onMakeOlder} 
+                        type="button" 
+                        className="btn btn-dark">Make me older!
+                </button>
+                <hr/>
+                <button onClick={this.props.greet} type="button" className="btn btn-dark">Greet</button>
+                <hr/>
+                <button onClick={this.onChangeLink} type="button" className="btn btn-dark">Change Link Name</button>
+                <input type="text" value={this.state.homeLink} onChange={this.onHandleChange} className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
             </div>
         )
     }
@@ -24,7 +89,8 @@ export class Home extends React.Component {
  
 Home.propTypes = {
     name: PropTypes.string,
-    age: PropTypes.string, 
-    user: PropTypes.object,
-    children: PropTypes.element.isRequired  
+    initialAge: PropTypes.number, 
+    greet: PropTypes.func,
+    initialLink: PropTypes.string,
+
 };
